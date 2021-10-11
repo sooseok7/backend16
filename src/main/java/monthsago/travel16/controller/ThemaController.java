bf89c87130c522e13ede51d7f9bc52024b86d7e1
joo-yongjae <0443jun@gmail.com>
2021-10-12 00:52:06 +0900

    테마별 api 음악제외

--- src/main/java/monthsago/travel16/controller/ThemaController.java
+++ src/main/java/monthsago/travel16/controller/ThemaController.java
@@ -1,9 +1,9 @@
 package monthsago.travel16.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import org.json.JSONObject;
-import org.json.XML;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.SerializationFeature;
+//import org.json.JSONObject;
+//import org.json.XML;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -35,7 +36,7 @@ public class ThemaController {
         urlBuilder.append("&" + URLEncoder.encode("resultType","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*JSON방식으로 호출 시 파라미터 resultType=json 입력*/
 
         URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection(); //url 오픈해라
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
         System.out.println("Response code: " + conn.getResponseCode());
@@ -52,7 +53,7 @@ public class ThemaController {
         }
         rd.close();
         conn.disconnect();
-        //System.out.println(sb.toString());
+        System.out.println(sb.toString());
         return sb.toString();
     }
 
@@ -66,13 +67,14 @@ public class ThemaController {
         urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
         urlBuilder.append("&" + URLEncoder.encode("MobileOS","UTF-8") + "=" + URLEncoder.encode("ETC", "UTF-8")); /*IOS (아이폰), AND (안드로이드), WIN (윈도우폰), ETC*/
         urlBuilder.append("&" + URLEncoder.encode("MobileApp","UTF-8") + "=" + URLEncoder.encode("AppTest", "UTF-8")); /*서비스명=어플명*/
-        urlBuilder.append("&" + URLEncoder.encode("crsKorNm","UTF-8") + "=" + URLEncoder.encode("밀양강", "UTF-8")); /*검색하고자 하는 코스명 (인코딩 필요)*/
-        urlBuilder.append("&" + URLEncoder.encode("routeIdx","UTF-8") + "=" + URLEncoder.encode("T_ROUTE_MNG0000000006", "UTF-8")); /*코스에 해당하는 길번호*/
+        urlBuilder.append("&" + URLEncoder.encode("crsKorNm","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*검색하고자 하는 코스명 (인코딩 필요)*/
+        urlBuilder.append("&" + URLEncoder.encode("routeIdx","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*코스에 해당하는 길번호*/
         urlBuilder.append("&" + URLEncoder.encode("crsLevel","UTF-8") + "=" + URLEncoder.encode("2", "UTF-8")); /*코스 난이도(1:하/2:중/3:상)*/
-        urlBuilder.append("&" + URLEncoder.encode("brdDiv","UTF-8") + "=" + URLEncoder.encode("DNBW", "UTF-8")); /*걷기/자전거 구분(DNWW : 걷기길, DNBW : 자전거길)*/
+        urlBuilder.append("&" + URLEncoder.encode("brdDiv","UTF-8") + "=" + URLEncoder.encode("DNWW", "UTF-8")); /*걷기/자전거 구분(DNWW : 걷기길, DNBW : 자전거길)*/
+        urlBuilder.append("&" + URLEncoder.encode("_type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));  //json리턴
 
         URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection(); //url 오픈해라
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
         System.out.println("Response code: " + conn.getResponseCode());
@@ -89,7 +91,7 @@ public class ThemaController {
         }
         rd.close();
         conn.disconnect();
-        //System.out.println(sb.toString());
+        System.out.println(sb.toString());
         return sb.toString();
     }
 
@@ -104,9 +106,10 @@ public class ThemaController {
         urlBuilder.append("&" + URLEncoder.encode("MobileOS","UTF-8") + "=" + URLEncoder.encode("ETC", "UTF-8")); /*IOS(아이폰), AND(안드로이드), WIN(윈도우폰),ETC(WEB이나 기타 등등)*/
         urlBuilder.append("&" + URLEncoder.encode("MobileApp","UTF-8") + "=" + URLEncoder.encode("AppTest", "UTF-8")); /*서비스명=어플명*/
         urlBuilder.append("&" + URLEncoder.encode("arrange","UTF-8") + "=" + URLEncoder.encode("A", "UTF-8")); /*A=촬영일, B=제목, C=수정일, D=조회수*/
+        urlBuilder.append("&" + URLEncoder.encode("_type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));  //json리턴
 
         URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection(); //url 오픈해라
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
         System.out.println("Response code: " + conn.getResponseCode());
@@ -123,22 +126,22 @@ public class ThemaController {
         }
         rd.close();
         conn.disconnect();
-        //System.out.println(sb.toString());
+        System.out.println(sb.toString());
         return sb.toString();
     }
 
-    @GetMapping("/getfestival")
-    public String getfestival() throws IOException {
+    @GetMapping("/getfood")
+    public String getfood() throws IOException {
 
-        StringBuilder urlBuilder = new StringBuilder("http://openapi.culture.go.kr/openapi/service/ArtKnwldgFstvlService/getArtKnwldgFstvl"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=ZyXbs3ggoxjvBgNnPZIbqucXxkimzEhCbTUf2TCX6PoRp%2BTOPYloYNtCYjVBaZzDEm8tpcfCZJW6RVoIRejKpg%3D%3D"); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + URLEncoder.encode("-", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
-        urlBuilder.append("&" + URLEncoder.encode("ctgry2","UTF-8") + "=" + URLEncoder.encode("J011", "UTF-8")); /*카테고리2 코드*/
-        urlBuilder.append("&" + URLEncoder.encode("ctgry3","UTF-8") + "=" + URLEncoder.encode("J333", "UTF-8")); /*카테고리3 코드*/
-        urlBuilder.append("&" + URLEncoder.encode("subject","UTF-8") + "=" + URLEncoder.encode("혜화동1번지", "UTF-8")); /*컨텐츠 제목*/
+        StringBuilder urlBuilder = new StringBuilder("https://api.odcloud.kr/api/15015962/v1/uddi:26d782a5-a334-454c-80f4-eeac6856b304_201909171114"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=ZyXbs3ggoxjvBgNnPZIbqucXxkimzEhCbTUf2TCX6PoRp%2BTOPYloYNtCYjVBaZzDEm8tpcfCZJW6RVoIRejKpg%3D%3D"); /*Service Key*/
+        //urlBuilder.append("&" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + URLEncoder.encode("-", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
+        urlBuilder.append("&" + URLEncoder.encode("page","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("perPage","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("resultType","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*JSON방식으로 호출 시 파라미터 resultType=json 입력*/
 
         URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection(); //url 오픈해라
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
         System.out.println("Response code: " + conn.getResponseCode());
@@ -155,8 +158,7 @@ public class ThemaController {
         }
         rd.close();
         conn.disconnect();
-        //System.out.println(sb.toString());
+        System.out.println(sb.toString());
         return sb.toString();
     }
-
 }
